885d5420d1c96f617670a7f4b73744cadc2a066a
kmk00 <73724318+kmk00@users.noreply.github.com>
2025-01-17 16:08:10 +0100

    Add daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,65 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.tsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.tsx",
     ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+    daisyui: {
+        themes: [
+            "dracula",
+            "dark",
+            "cmyk",
+            {
+                customTheme: {
+                    primary: "#fc0070",
+
+                    "primary-content": "#160004",
+
+                    secondary: "#0073ff",
+
+                    "secondary-content": "#000516",
+
+                    accent: "#00b0ff",
+
+                    "accent-content": "#000b16",
+
+                    neutral: "#100700",
+
+                    "neutral-content": "#c9c6c2",
+
+                    "base-100": "#202127",
+
+                    "base-200": "#1a1b20",
+
+                    "base-300": "#15161a",
+
+                    "base-content": "#cdcdcf",
+
+                    info: "#00beff",
+
+                    "info-content": "#000d16",
+
+                    success: "#00ec96",
+
+                    "success-content": "#001308",
+
+                    warning: "#f17e00",
+
+                    "warning-content": "#140500",
+
+                    error: "#ff656a",
+
+                    "error-content": "#160304",
+                },
             },
-        },
+        ],
     },
 
-    plugins: [forms],
+    plugins: [forms, require("daisyui")],
 };
